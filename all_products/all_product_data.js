eab783b4574dc9186873be532f62117710a87ccd
Anna Thorslund <anna728x@stud.kea.dk>
2021-06-03 14:05:00 +0200

    just a quick safe

--- all_products/all_product_data.js
+++ all_products/all_product_data.js
@@ -25,3 +25,47 @@ function fetchAll() {
         })
 
 }
+
+
+
+const url = "https://kea21s-9328.restdb.io/rest/Products?"
+
+/*API key*/
+const options = {
+  headers: {
+    "x-apikey": "602e2b3b5ad3610fb5bb6298",
+  },
+};
+
+fetch(url, {
+  method: "GET",
+  headers: {
+    "x-apikey": "602e2b3b5ad3610fb5bb6298",
+  },
+})
+  .then((res) => res.json())
+  .then((response) => {
+    handleProducts(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+  function handleProducts (data) {
+    // console.log (data);
+    data.forEach (showProducts);
+  }
+
+
+function showProducts(products) {
+    console.log(products);
+    const template = document.querySelector("#productTemplate").textContent;
+
+const copy = template.cloneNode(true);
+
+    document.querySelector("h1.productName").textContent = products.name;
+    document.querySelector("img.productimg").src = products.photoOne;
+    document.querySelector("p.price").textContent = `${products.price} DKK`
+
+  }
+  
