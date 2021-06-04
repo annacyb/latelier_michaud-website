7815d8b907fd00ec1a5e12eccca76e4612e26c94
anasofich <castellanos_chopas@hotmail.com>
2021-06-03 22:07:42 -0500

    adjusted stuff

--- one_product/productView.js
+++ one_product/productView.js
@@ -44,8 +44,8 @@ function handleProducts(product) {
   }
 
   document.querySelector(".addCart").addEventListener("click", () => {
-    //alert("hey!");
     //console.log(bags);
+    alert("Added to cart");
     CART.add(product);
   });
 }
@@ -193,31 +193,55 @@ const CART = {
   updateDOM() {
     const cartcontentEl = document.querySelector(".cartContent");
     cartcontentEl.innerHTML = "";
-    CART.contents.forEach((element) => {
-      const tempItem = document.querySelector("#cartTemplate").content;
-      //console.log(element);
-      const itemCopy = tempItem.cloneNode(true);
 
-      const bagID = element._id;
-      itemCopy.querySelector(".bagName").textContent = element.name;
-      itemCopy.querySelector("label").setAttribute("for", "fid-" + bagID);
+    if (CART.contents.length === 0) {
+      cartcontentEl.innerHTML = "<h2>The cart is empty</h2>";
+      document.querySelector(".lineL").classList.add("hidden");
+      document.querySelector(".orderPrice").classList.add("hidden");
+      document.querySelector(".itemsTwo").classList.add("hidden");
+    } else {
+      CART.contents.forEach((element) => {
+        const tempItem = document.querySelector("#cartTemplate").content;
+        //console.log(element);
+        const itemCopy = tempItem.cloneNode(true);
 
-      itemCopy.querySelector("input").id += bagID;
-      itemCopy.querySelector("input").name += bagID;
-      itemCopy.querySelector("input").value = element.qty;
-      itemCopy.querySelector("input").addEventListener("change", () => {
-        const itemQty = itemCopy.querySelector("input").valueAsNumber;
-        console.log("element");
-        console.log(element.qty);
-        CART.update(element);
+        const bagID = element._id;
+        itemCopy.querySelector(".bagName").textContent = element.name;
+        itemCopy.querySelector("label").setAttribute("for", "fid-" + bagID);
+
+        const minusBtn = itemCopy.querySelector(".minus");
+        minusBtn.addEventListener("click", () => {
+          CART.minusOne(bagID);
+        });
+
+        itemCopy.querySelector("input").id += bagID;
+        itemCopy.querySelector("input").name += bagID;
+        itemCopy.querySelector("input").value = element.qty;
+
+        itemCopy.querySelector("input").addEventListener("blur", () => {
+          const itemQty = itemCopy.querySelector("input").valueAsNumber;
+          element.qty = itemQty;
+          console.log("element");
+          console.log(element);
+          CART.update(element);
+        });
+
+        itemCopy.querySelector("input").addEventListener("focus", (e) => {
+          e.target.select();
+        });
+
+        const plusBtn = itemCopy.querySelector(".plus");
+        plusBtn.addEventListener("click", () => {
+          CART.plusOne(bagID);
+        });
+
+        itemCopy.querySelector("img").src = element.photoOne;
+        itemCopy.querySelector(".bagColor").textContent = element.material;
+        itemCopy.querySelector(".bagproductPrice").textContent = element.price;
+
+        cartcontentEl.appendChild(itemCopy);
       });
-
-      itemCopy.querySelector("img").src = element.photoOne;
-      itemCopy.querySelector(".bagColor").textContent = element.material;
-      itemCopy.querySelector(".bagproductPrice").textContent = element.price;
-
-      cartcontentEl.appendChild(itemCopy);
-    });
+    }
   },
   add(obj) {
     const index = CART.contents.findIndex((element) => element._id == obj._id);
@@ -235,57 +259,29 @@ const CART = {
   },
   update(obj) {
     const index = CART.contents.findIndex((element) => element._id == obj._id);
-    const inputEl = document.querySelector("#fid-" + obj._id);
-    CART.contents[index].qty = inputEl.valueAsNumber;
+    /*const inputEl = document.querySelector("#fid-" + obj._id);
+    CART.contents[index].qty = inputEl.valueAsNumber;*/
+
+    if (obj.qty === 0) {
+      CART.contents.splice(index, 1);
+    } else {
+      CART.contents[index].qty = obj.qty;
+    }
+
     CART.sync();
+  },
+  minusOne(bagID) {
+    const indexObj = CART.contents.find((element) => element._id == bagID);
+    indexObj.qty--;
+    console.log(indexObj);
+    CART.update(indexObj);
+  },
+  plusOne(bagID) {
+    const indexObj = CART.contents.find((element) => element._id == bagID);
+    indexObj.qty++;
+    console.log(indexObj);
+    CART.update(indexObj);
   },
 };
 
 CART.init();
-
-/*RECOMMENDATIONS*/
-
-/*API key*/
-
-const url1 = "https://kea21s-9328.restdb.io/rest/products?max=5";
-const options1 = {
-  headers: {
-    "x-apikey": "602e2b3b5ad3610fb5bb6298",
-  },
-};
-
-fetch(url1, options1)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-
-  .then((dataa) => {
-    handleRec(dataa);
-  })
-  .catch((e) => {
-    console.error("an error occured:", e.message);
-  });
-
-function handleRec(data) {
-  console.log(data);
-  data.forEach((e) => showRec(e));
-}
-
-function showRec(rec) {
-  //grab the template
-  const template = document.querySelector("#recBags").content;
-  //clone it
-  const copy = template.cloneNode(true);
-  //change the content
-  copy.querySelector(".p").textContent = rec.name;
-  copy.querySelector(".price").textContent = `${rec.price} DKK`;
-  copy.querySelector("img").src = rec.photoOne;
-
-  //grab the parent
-  const parent = document.querySelector(".recProducts");
-  //append
-  parent.appendChild(copy);
-}
